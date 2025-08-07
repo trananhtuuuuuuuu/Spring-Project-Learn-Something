502e5228d9da77adcd9c37c44b19e8375104cd6c
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-07 09:06:40 +0700

    User temporary Entity (not completed)

---
+++ src/main/java/com/projectToLearn/springProject/domain/User.java
@@ -0,0 +1,55 @@
+package com.projectToLearn.springProject.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name="user")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name="first_name")
+  private String firstName;
+
+  @Column(name="last_name")
+  private String lastName;
+
+  @Column(name="email", unique = true)
+  private String email;
+
+  @Column(name="password")
+  private String password;
+
+  public User(String firstName, 
+  String lastName, 
+  String email, 
+  String password) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+  }
+
+  @Override
+  public String toString() {
+    return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", password="
+        + password + "]";
+  }
+
+  
+  
+}
