f3756fe36bf169296595960fe7e8f776bebf005d
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-07 14:58:03 +0700

    Updated Category Repository

--- src/main/java/com/projectToLearn/springProject/repository/CategoryRepository.java
+++ src/main/java/com/projectToLearn/springProject/repository/CategoryRepository.java
@@ -2,10 +2,13 @@ package com.projectToLearn.springProject.repository;
 
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projectToLearn.springProject.domain.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
   boolean existsByName(String name);
+  List<Category> findByName(String name);
 }
