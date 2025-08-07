5b0474c20c3144fd2bc2ea558e3d429412c79040
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-07 15:11:48 +0700

    add functions for ProductRepository

--- src/main/java/com/projectToLearn/springProject/repository/ProductRepository.java
+++ src/main/java/com/projectToLearn/springProject/repository/ProductRepository.java
@@ -2,8 +2,44 @@ package com.projectToLearn.springProject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import com.projectToLearn.springProject.domain.Product;
+import java.util.List;
+
+
+
+
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-  
+
+
+  List<Product> findByBrand(String brand);
+
+
+
+
+  List<Product> findByCategoryName(String categoryName);
+
+
+
+  List<Product> findByCategoryNameAndBrand(String categoryName, String brand);
+
+
+
+
+  List<Product> findByName(String name);
+
+
+
+  List<Product> findByBrandAndName(String brand, String name);
+
+
+
+
+  Long countByBrandAndName(String brand, String name);
+
+
+
+
+
 }
