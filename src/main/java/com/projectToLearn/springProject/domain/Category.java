ed7b773d1df1f6b544065172ac25e2f18ecfc203
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-07 13:40:55 +0700

    create Category Entity

---
+++ src/main/java/com/projectToLearn/springProject/domain/Category.java
@@ -0,0 +1,40 @@
+package com.projectToLearn.springProject.domain;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name="category")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name="name")
+  private String name;
+
+  @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+  private List<Product> products;
+
+  public Category(String name){
+    this.name = name;
+  }
+  
+}
