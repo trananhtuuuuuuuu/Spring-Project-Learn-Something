235499c7479c19926da9e5c3ab38c617ee66f9a6
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-07 14:36:45 +0700

    add existsByName function

--- src/main/java/com/projectToLearn/springProject/repository/CategoryRepository.java
+++ src/main/java/com/projectToLearn/springProject/repository/CategoryRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.projectToLearn.springProject.domain.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-  
+  boolean existsByName(String name);
 }
