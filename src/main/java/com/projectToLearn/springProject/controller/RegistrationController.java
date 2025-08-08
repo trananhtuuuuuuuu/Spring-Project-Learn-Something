8ab273d146085ae426ac6b061d9d4b1c95489760
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-08 17:15:02 +0700

    created a endpoint create a user with role

--- src/main/java/com/projectToLearn/springProject/controller/RegistrationController.java
+++ src/main/java/com/projectToLearn/springProject/controller/RegistrationController.java
@@ -1,8 +1,52 @@
 package com.projectToLearn.springProject.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projectToLearn.springProject.domain.Role;
+import com.projectToLearn.springProject.domain.User;
+import com.projectToLearn.springProject.dto.UserDto;
+import com.projectToLearn.springProject.response.ApiResponse;
+import com.projectToLearn.springProject.service.role.RoleService;
+import com.projectToLearn.springProject.service.user.UserService;
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class RegistrationController {
   
+  private final UserService userService;
+  private final RoleService roleService;
+
+  @PostMapping("/admin/registrations")
+  public ResponseEntity<ApiResponse<UserDto>> postCreateUser(
+    @RequestBody UserDto userDto
+  ){
+    User user = new User(userDto.getFirstName(),
+    userDto.getLastName(),
+    userDto.getEmail(),
+    userDto.getPassword());
+    Role role = this.roleService.getRoleByName(userDto.getRoleName());
+    user.setRole(role);
+    User savedUser = this.userService.saveUser(user);
+    userDto.setFirstName(savedUser.getFirstName()); // avoid warning for savedUser varitable
+    return ResponseEntity.status(HttpStatus.CREATED).body(
+      new ApiResponse<UserDto>(
+      "successful",
+      userDto
+    )
+    );
+  }
+
+
+
+
+
+
+
+
+
 }
