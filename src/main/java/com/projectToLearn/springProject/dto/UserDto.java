50ede4b3b1788f61c6030972ee6ff12ebf06c6f6
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-08 13:57:32 +0700

    userDto created

--- src/main/java/com/projectToLearn/springProject/dto/UserDto.java
+++ src/main/java/com/projectToLearn/springProject/dto/UserDto.java
@@ -6,7 +6,31 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserDto {
+
+  Long id;
+  String firstName;
+  String lastName;
   String email;
   String password;
+
+  public UserDto(
+    String email,
+    String password
+  ){
+    this.email = email;
+    this.password = password;
+  }
+
+  public UserDto(
+    Long id,
+    String firstName,
+    String lastName,
+    String email
+  ){
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
 
 }
