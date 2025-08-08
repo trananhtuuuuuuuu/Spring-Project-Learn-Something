1b35c34abfda21a79a066e656320d9815d322f79
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-08 17:15:58 +0700

    updated UserDto class

--- src/main/java/com/projectToLearn/springProject/dto/UserDto.java
+++ src/main/java/com/projectToLearn/springProject/dto/UserDto.java
@@ -1,10 +1,12 @@
 package com.projectToLearn.springProject.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class UserDto {
 
   Long id;
@@ -12,25 +14,45 @@ public class UserDto {
   String lastName;
   String email;
   String password;
+  String roleName;
 
   public UserDto(
     String email,
-    String password
+    String password,
+    String roleName
   ){
     this.email = email;
     this.password = password;
+    this.roleName = roleName;
   }
 
   public UserDto(
     Long id,
     String firstName,
     String lastName,
-    String email
+    String email,
+    String roleName
   ){
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
+    this.roleName = roleName;
+  }
+
+
+  public UserDto(
+    String firstName,
+    String lastName,
+    String email,
+    String password,
+    String roleName
+  ){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.roleName = roleName;
   }
 
 }
