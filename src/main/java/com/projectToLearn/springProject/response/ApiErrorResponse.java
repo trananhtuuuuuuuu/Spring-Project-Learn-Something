61927892c238adfbb91792b2f1bbac0383a0e7c0
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-08 16:58:44 +0700

    config response API

---
+++ src/main/java/com/projectToLearn/springProject/response/ApiErrorResponse.java
@@ -0,0 +1,15 @@
+package com.projectToLearn.springProject.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiErrorResponse {
+  private Object error;
+  private String message;
+}
---
+++ src/main/java/com/projectToLearn/springProject/response/ApiResponse.java
@@ -0,0 +1,15 @@
+package com.projectToLearn.springProject.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+  private Object message;
+  private T data;
+}
