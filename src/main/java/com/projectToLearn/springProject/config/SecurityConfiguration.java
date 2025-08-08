360ce7a0dbfbd3087823b578c2ce014da4caa86b
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-08 17:14:18 +0700

    config SecurityConfiguration class

--- src/main/java/com/projectToLearn/springProject/config/SecurityConfiguration.java
+++ src/main/java/com/projectToLearn/springProject/config/SecurityConfiguration.java
@@ -1,10 +1,43 @@
 package com.projectToLearn.springProject.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration {
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean 
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    http.authorizeHttpRequests(
+      requests ->requests
+      .requestMatchers(HttpMethod.POST, "/admin/**").permitAll()
+      .requestMatchers("/", "/admin").permitAll()
+      .anyRequest().authenticated()
+    )
+    .csrf(csrf -> csrf.disable())
+    .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            );
+    return http.build();
+  }
+
+
+
+
+  
+
   
 }
