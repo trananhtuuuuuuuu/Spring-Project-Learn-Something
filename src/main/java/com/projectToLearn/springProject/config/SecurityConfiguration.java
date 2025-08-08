9b04440b729b9e165019e6ff6e3cf6af146fa7ee
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-08 13:58:59 +0700

    created Spring securityConfiguration

---
+++ src/main/java/com/projectToLearn/springProject/config/SecurityConfiguration.java
@@ -0,0 +1,10 @@
+package com.projectToLearn.springProject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfiguration {
+  
+}
