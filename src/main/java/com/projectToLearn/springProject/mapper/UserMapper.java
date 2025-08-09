5b882f723374ab3e2716078837582acf0a27d62a
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-09 20:47:25 +0700

    add userToUserDtoResponse function

--- src/main/java/com/projectToLearn/springProject/mapper/UserMapper.java
+++ src/main/java/com/projectToLearn/springProject/mapper/UserMapper.java
@@ -1,8 +1,13 @@
 package com.projectToLearn.springProject.mapper;
 
+import org.springframework.stereotype.Service;
+
 import com.projectToLearn.springProject.domain.User;
 import com.projectToLearn.springProject.dto.UserDto;
+import com.projectToLearn.springProject.dto.UserDtoResponse;
 
+
+@Service
 public class UserMapper {
   public User userDtoToUser(
     UserDto userDto){
@@ -25,5 +30,13 @@ public class UserMapper {
       user.getEmail(),
       user.getRole().getName()
     );
+  }
+
+  public UserDtoResponse userToUserDtoResponse(User user){
+    return new UserDtoResponse(
+      user.getFirstName(), 
+      user.getLastName(), 
+      user.getEmail(), 
+      user.getRole().getName());
   }
 }
