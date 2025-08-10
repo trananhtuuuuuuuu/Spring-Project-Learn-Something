f1beeec8a253d4b42e54fa3751c633492d709609
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-10 16:09:00 +0700

    add getUsersByEmail endpoint

--- src/main/java/com/projectToLearn/springProject/controller/UserController.java
+++ src/main/java/com/projectToLearn/springProject/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -55,7 +56,20 @@ public class UserController {
 
     apiResponse.setMessage(HttpStatus.OK.getReasonPhrase());
     apiResponse.setData(this.userMapper.userToUserDtoResponse(user));
+
+    return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+  }
+
+
+  @GetMapping("/api/admin/users/email")
+  public ResponseEntity<Object> getUsersByEmail(@RequestParam String email){
     
+    ApiResponse<UserDtoResponse> apiResponse = new ApiResponse<UserDtoResponse>();
+    User user = this.userService.getUserByEmail(email);
+
+    apiResponse.setMessage(HttpStatus.OK.getReasonPhrase());
+    apiResponse.setData(this.userMapper.userToUserDtoResponse(user));
+
     return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
   }
   
