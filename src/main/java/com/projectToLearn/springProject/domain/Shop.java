bbba3690036f09c247a5082a8867baa6a7f19109
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-10 12:04:22 +0700

    created Shop class

---
+++ src/main/java/com/projectToLearn/springProject/domain/Shop.java
@@ -0,0 +1,53 @@
+package com.projectToLearn.springProject.domain;
+
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="shop")
+public class Shop {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name="shop_name")
+  private String name;
+
+  @Column(name="shop_address")
+  private String address;
+
+  @Column(name="date_original")
+  private String dateOriginal;
+
+  public Shop(String name,
+  String address,
+  String Date){
+    this.name = name;
+    this.address = address;
+    this.dateOriginal = Date;
+  }
+
+  @OneToMany(mappedBy = "shop", cascade = CascadeType.ALL)
+  private List<ProductShop> productShops;
+
+
+  
+}
