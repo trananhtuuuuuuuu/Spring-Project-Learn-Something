fdcd5ebc3a6d8a2f8d22029576c7031e8b535186
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-10 17:22:05 +0700

    need updated again for update function user

--- src/main/java/com/projectToLearn/springProject/controller/UserController.java
+++ src/main/java/com/projectToLearn/springProject/controller/UserController.java
@@ -2,13 +2,17 @@ package com.projectToLearn.springProject.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+
 import com.projectToLearn.springProject.domain.User;
+
 import com.projectToLearn.springProject.dto.UserDtoResponse;
+import com.projectToLearn.springProject.dto.UserDtoUpdate;
+import com.projectToLearn.springProject.exception.EmailNotValidException;
 import com.projectToLearn.springProject.mapper.UserMapper;
 import com.projectToLearn.springProject.response.ApiResponse;
 import com.projectToLearn.springProject.service.user.UserService;
 
-import lombok.AllArgsConstructor;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,16 +22,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
 
 
 @RestController
-@AllArgsConstructor
+//@AllArgsConstructor
 public class UserController {
   private final UserService userService;
   private final UserMapper userMapper;
+
+  public UserController(UserService userService,
+  UserMapper userMapper){
+    this.userService = userService;
+    this.userMapper = userMapper;
+  }
 
   @GetMapping("/api/admin/users")
   public ResponseEntity<Object> getAllUsers() {
@@ -71,6 +84,26 @@ public class UserController {
     apiResponse.setData(this.userMapper.userToUserDtoResponse(user));
 
     return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+  }
+
+  @PutMapping("/api/admin/users/{userId}")
+  public ResponseEntity<Object> putUpdateUsersById(@PathVariable Long userId, 
+  @RequestBody UserDtoUpdate userDtoUpdate) {
+
+    if(!this.userService.isValidEmail(userDtoUpdate.getEmail())){
+      throw new EmailNotValidException("Email not valid");
+    }  
+    
+    User userConverted = this.userMapper.userDtoUpdateToUser(userDtoUpdate);
+
+    User userUpdated = this.userService.updateUserById(userId, userConverted);
+
+    ApiResponse<UserDtoResponse> apiResponse = new ApiResponse<UserDtoResponse>();
+
+    apiResponse.setMessage(HttpStatus.OK.getReasonPhrase());
+    apiResponse.setData(this.userMapper.userToUserDtoResponse(userUpdated));
+
+    return ResponseEntity.ok(apiResponse);
   }
   
   
