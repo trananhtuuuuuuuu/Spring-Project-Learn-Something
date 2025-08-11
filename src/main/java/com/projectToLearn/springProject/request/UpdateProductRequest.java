99d4de33be9126074d224f12a21bd82b0cee4e0c
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-11 16:13:04 +0700

    created UpdateProductRequest class for update function

--- src/main/java/com/projectToLearn/springProject/request/UpdateProductRequest.java
+++ src/main/java/com/projectToLearn/springProject/request/UpdateProductRequest.java
@@ -1,5 +1,39 @@
 package com.projectToLearn.springProject.request;
 
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateProductRequest {
-  
+  @NotNull
+  @NotEmpty
+  private String name;
+
+  @NotNull
+  @NotEmpty
+  private String brand;
+
+  @NotNull(message = "Price is required")
+  private BigDecimal price;
+
+  @NotNull(message = "Inventory is required")
+  private Integer inventory;
+
+  @NotNull
+  @NotEmpty
+  private String description;
+
+  @NotNull
+  @NotEmpty
+  private String categoryName;
 }
