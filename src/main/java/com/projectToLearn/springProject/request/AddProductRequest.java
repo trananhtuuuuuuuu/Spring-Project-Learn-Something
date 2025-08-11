582f19bfa62d7728d41ab93c78114661550a9905
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-11 09:40:26 +0700

    created AddProductRequest

--- src/main/java/com/projectToLearn/springProject/request/AddProductRequest.java
+++ src/main/java/com/projectToLearn/springProject/request/AddProductRequest.java
@@ -1,8 +1,41 @@
 package com.projectToLearn.springProject.request;
 
-import lombok.Data;
+import java.math.BigDecimal;
 
-@Data
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddProductRequest {
-  
+
+  @NotNull
+  @NotEmpty
+  private String name;
+
+  @NotNull
+  @NotEmpty
+  private String brand;
+
+  @NotNull(message = "Price is required")
+  private BigDecimal price;
+
+  @NotNull(message = "Inventory is required")
+  private int inventory;
+
+  @NotNull
+  @NotEmpty
+  private String description;
+
+  @NotNull
+  @NotEmpty
+  private String categoryName;
 }
