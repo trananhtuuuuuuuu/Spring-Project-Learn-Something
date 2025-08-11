a3bc9bad1ec3d1c1d8bb3c855b0db6a901c06b8d
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-11 09:38:19 +0700

    src\main\java\com\projectToLearn\springProject\dto\ProductDto.java

--- src/main/java/com/projectToLearn/springProject/dto/ProductDto.java
+++ src/main/java/com/projectToLearn/springProject/dto/ProductDto.java
@@ -1,5 +1,22 @@
 package com.projectToLearn.springProject.dto;
 
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDto {
-  
+  private Long id;
+  private String name;
+  private String brand;
+  private BigDecimal price;
+  private int inventory;
+  private String description;
+  private String categoryName;
 }
