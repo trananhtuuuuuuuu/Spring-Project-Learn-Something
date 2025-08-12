f73b0d6c4f8aa988fb58ee4505b6540a43ff655d
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-12 16:21:06 +0700

    ProductShopRepository

--- src/main/java/com/projectToLearn/springProject/repository/ProductShopRepository.java
+++ src/main/java/com/projectToLearn/springProject/repository/ProductShopRepository.java
@@ -3,10 +3,12 @@ package com.projectToLearn.springProject.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.projectToLearn.springProject.domain.Product;
 import com.projectToLearn.springProject.domain.ProductShop;
+import com.projectToLearn.springProject.domain.Shop;
 
 
 @Repository
 public interface ProductShopRepository extends JpaRepository<ProductShop, Long> {
-  
+  boolean existsByProductAndShop(Product product, Shop shop);
 }
