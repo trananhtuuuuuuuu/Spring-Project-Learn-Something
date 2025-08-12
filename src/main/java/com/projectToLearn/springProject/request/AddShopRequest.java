f62208d8abcdf9ea06eef810b71b4f6bec2f13de
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-12 12:37:38 +0700

    updated AddShopRequest class

--- src/main/java/com/projectToLearn/springProject/request/AddShopRequest.java
+++ src/main/java/com/projectToLearn/springProject/request/AddShopRequest.java
@@ -2,12 +2,10 @@ package com.projectToLearn.springProject.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class AddShopRequest {
   private String name;
