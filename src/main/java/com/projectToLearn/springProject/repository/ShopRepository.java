e74294ddba48dadb6fcd4961a6986013ef298d19
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-12 11:34:48 +0700

    updated ShopRepository

---
+++ src/main/java/com/projectToLearn/springProject/repository/ShopRepository.java
@@ -0,0 +1,21 @@
+package com.projectToLearn.springProject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.projectToLearn.springProject.domain.Shop;
+import java.util.List;
+
+
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+  List<Shop> findByAddress(String address);
+  List<Shop> findByName(String name);
+
+  @Query("select s from shop s where year(s.dateOriginal) = :year")
+  List<Shop> findByYear(@Param("year") int year);
+
+  @Query("select s from shop s where year(s.dateOriginal) = :year And month(s.dateOriginal) = :month")
+  List<Shop> findByMonthAndYear(@Param("month") int month,
+  @Param("year") int year);
+}
